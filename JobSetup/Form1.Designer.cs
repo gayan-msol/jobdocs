88895403e8752391a9f40236b1d55539e136fa75
Gayan Wijesinghe <gayan@mailingsolutions.net.au>
2022-06-10 09:02:41 +0800

    Job setu project

---
+++ JobSetup/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace JobSetup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxJobNo = new System.Windows.Forms.TextBox();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.txtArt = new System.Windows.Forms.TextBox();
+            this.txtOldJobNo = new System.Windows.Forms.TextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxJobNo
+            // 
+            this.textBoxJobNo.Location = new System.Drawing.Point(98, 37);
+            this.textBoxJobNo.Name = "textBoxJobNo";
+            this.textBoxJobNo.Size = new System.Drawing.Size(100, 20);
+            this.textBoxJobNo.TabIndex = 0;
+            this.textBoxJobNo.TextChanged += new System.EventHandler(this.textBoxJobNo_TextChanged);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(204, 34);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 1;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(98, 98);
+            this.txtData.Multiline = true;
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(443, 46);
+            this.txtData.TabIndex = 2;
+            this.txtData.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtArt
+            // 
+            this.txtArt.Location = new System.Drawing.Point(98, 157);
+            this.txtArt.Multiline = true;
+            this.txtArt.Name = "txtArt";
+            this.txtArt.Size = new System.Drawing.Size(443, 47);
+            this.txtArt.TabIndex = 3;
+            // 
+            // txtOldJobNo
+            // 
+            this.txtOldJobNo.Location = new System.Drawing.Point(98, 63);
+            this.txtOldJobNo.Name = "txtOldJobNo";
+            this.txtOldJobNo.Size = new System.Drawing.Size(100, 20);
+            this.txtOldJobNo.TabIndex = 4;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(429, 210);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(112, 47);
+            this.btnCopy.TabIndex = 5;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Job Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Data Folder";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Art Folder";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Old Job Number";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnImport;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 290);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.txtOldJobNo);
+            this.Controls.Add(this.txtArt);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.textBoxJobNo);
+            this.Name = "Form1";
+            this.Text = "Job Setup";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxJobNo;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.TextBox txtArt;
+        private System.Windows.Forms.TextBox txtOldJobNo;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ JobSetup/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dolphin;
+
+namespace JobSetup
+{
+    public partial class Form1 : Form
+    {
+        Job importedJob = new Job();
+        string jobName = "";
+        string customer = "";
+        string jobNo = "";
+        string jobDirectoryData = "";
+        string jobDirectoryArt = "";
+        string oldJobNo = "";
+        string newJobDirectoryData = "";
+        string newJobDirectoryArt = "";
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            importedJob = Job.GetJob(textBoxJobNo.Text);
+            if (importedJob != null)
+            {
+
+
+                jobName= importedJob.JobName;
+                customer = importedJob.Customer;
+                jobNo = importedJob.JobNumber;
+                txtData.Text  = importedJob.DataFolder;
+                txtArt.Text = importedJob.ArtworkFolder;
+                oldJobNo = "";
+               if(!string.IsNullOrWhiteSpace(txtData.Text))
+                {
+                    newJobDirectoryData = $@"{Directory.GetParent(txtData.Text).FullName}\JOB {jobNo} {jobName.ToUpper()}";
+                    txtOldJobNo.Text = Path.GetFileName(txtData.Text).Split(' ')[1];
+                    
+                }
+
+                if (!string.IsNullOrWhiteSpace(txtArt.Text))
+                {
+                    txtOldJobNo.Text = Path.GetFileName(txtArt.Text).Split(' ')[1];
+                     newJobDirectoryArt = $@"{Directory.GetParent(txtArt.Text).FullName}\JOB {jobNo} {jobName.ToUpper()}";
+                 
+                }
+
+
+
+
+
+
+            }
+
+        }
+
+        private  void CopyDirectory(string sourceDir, string destinationDir)
+        {
+            // Get information about the source directory
+            var dir = new DirectoryInfo(sourceDir);
+
+            // Check if the source directory exists
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+           
+            // Cache directories before we start copying
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // Create the destination directory
+            Directory.CreateDirectory(destinationDir);
+
+            List<string> extList = new List<string>() { ".xlsx", ".xls", ".txt", ".csv", ".pdf", ".lpf", ".ps" };
+
+            foreach (FileInfo file in dir.GetFiles())
+            {
+
+                if (!extList.Contains(file.Extension))
+                {
+
+                    string targetFilePath = Path.Combine(destinationDir, file.Name.Replace(oldJobNo, jobNo));
+                    file.CopyTo(targetFilePath);
+                }
+            }
+
+                   
+                foreach (DirectoryInfo subDir in dirs)
+                {
+                    string newDestinationDir = Path.Combine(destinationDir, subDir.Name.Replace(oldJobNo, jobNo));
+                    CopyDirectory(subDir.FullName, newDestinationDir);
+                }
+            
+        }
+
+        private void textBoxJobNo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtData.Text) && Directory.Exists(txtData.Text))
+            {
+                CopyDirectory(txtData.Text, newJobDirectoryData);
+            }
+            if (!string.IsNullOrWhiteSpace(txtArt.Text) && Directory.Exists(txtArt.Text))
+            {
+                CopyDirectory(txtArt.Text, newJobDirectoryArt);
+            }
+
+
+        }
+    }
+}
